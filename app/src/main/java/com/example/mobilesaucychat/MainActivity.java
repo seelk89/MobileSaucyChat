75cc925d6b889061e43da629e0024f3349c3ef29
seelk89 <seelk.89@gmail.com>
2019-04-29 10:57:21 +0200

    login seemingly works

--- app/src/main/java/com/example/mobilesaucychat/MainActivity.java
+++ app/src/main/java/com/example/mobilesaucychat/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.mobilesaucychat;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,15 +8,18 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etEmail, etPassword;
     FirebaseAuth firebaseAuth;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnSignIn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onClickSignIn();
+                onClickSignIn(etEmail.getText().toString(), etPassword.getText().toString());
             }
         });
 
@@ -43,28 +45,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /*
         if(firebaseAuth.getCurrentUser()!= null){
             startActivity(new Intent(getApplicationContext(), ChatRoomActivity.class));
         }
+        */
     }
 
-    private void onClickSignIn() {
-        Intent newActivity = new Intent(this, ChatRoomActivity.class);
-        startActivity(newActivity);
+    private void onClickSignIn(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Successfully signed in.", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Failed to sign in.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void onClickSignUpWithEmailAndPassword(String email, String password) {
         if(TextUtils.isEmpty(email)){
-            Toast.makeText(getApplicationContext(),"Please fill in the required email field",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"Please fill in the required email field", Toast.LENGTH_SHORT).show();
             return;
         }
 
         if(TextUtils.isEmpty(password)){
-            Toast.makeText(getApplicationContext(),"Please fill in the required password field",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"Please fill in the required password field", Toast.LENGTH_SHORT).show();
         }
 
         if(password.length() < 6){
-            Toast.makeText(getApplicationContext(),"Password must be at least 6 characters",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
         }
 
         firebaseAuth.createUserWithEmailAndPassword(email, password)
@@ -73,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(getApplicationContext(),"Successfully created account",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), ChatRoomActivity.class));
+                            // startActivity(new Intent(getApplicationContext(), ChatRoomActivity.class));
                             finish();
                         }
                         else{
